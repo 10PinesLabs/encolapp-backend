eadedce78ac2d35602a5e95dadde7cdb4593934f
Dario L Garcia <dario.garcia@10pines.com>
2018-01-27 14:48:28 -0300

    autorizar cors

--- src/main/java/com/tenpines/encolapp/ConfiguracionDeSpring.java
+++ src/main/java/com/tenpines/encolapp/ConfiguracionDeSpring.java
@@ -2,6 +2,9 @@ package com.tenpines.encolapp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 
 /**
  * Date: 27/01/18 - 13:17
@@ -12,5 +15,20 @@ public class ConfiguracionDeSpring {
   @Bean
   public Salon salon() {
     return Salon.create();
+  }
+
+
+  @Bean
+  public WebFluxConfigurer corsConfigurer() {
+    return new WebFluxConfigurerComposite() {
+
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+          .addMapping("/**")
+          .allowedOrigins("*")
+          .allowedMethods("*");
+      }
+    };
   }
 }
